8a792d6cd09ff419f3cf136207c34a0b55d48464
olimarel <olimarel@gmail.com>
2023-02-14 10:30:22 +0000

    Added isEmpty() method

--- src/main/java/is/hi/hbv202g/ass5/IntStack.java
+++ src/main/java/is/hi/hbv202g/ass5/IntStack.java
@@ -33,6 +33,7 @@ public class IntStack {
 
     /**
      * Return the capacity of the stack.
+     *
      * @return the capacity of the stack
      */
     public int getCapacity() {
@@ -42,6 +43,7 @@ public class IntStack {
     /**
      * Returns whether the stack is full.
      * A further push would throw an exception.
+     *
      * @return true if the stack is full, false if the stack is not full
      */
     public boolean isFull() {
@@ -53,9 +55,13 @@ public class IntStack {
     }
 
     // TODO: Add isEmpty() method
+    public Boolean isEmpty() {
+        return nextPushLocation == 0;
+    }
 
     /**
      * Push on int on the stack.
+     *
      * @param element The int to be pushed on the stack
      * @throws ArrayIndexOutOfBoundsException If the stack was already full
      */
@@ -65,6 +71,7 @@ public class IntStack {
 
     /**
      * Pops an int from the stack
+     *
      * @return The poppsed int
      * @throws ArrayIndexOutOfBoundsException If the stack was already empty
      */
